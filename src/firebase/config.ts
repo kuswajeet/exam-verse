594aa4471f12da82f72bdef9d69fb5889eca3827
kuswajeet Gupta <kuswajeetgupta@gmail.com>
2026-01-01 13:43:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8465754929-f1e1f",
+  "appId": "1:738536341108:web:b4f668c1c34dad9c5d9b21",
+  "apiKey": "AIzaSyAa9Q9ugfETMSFnVX99sBcs1i25KEurzIk",
+  "authDomain": "studio-8465754929-f1e1f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "738536341108"
+};
