7a2d77967fd34306b8b06c49988544a4d7fb2c80
kuswajeet Gupta <kuswajeetgupta@gmail.com>
2026-01-03 03:22:13 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -2,28 +2,7 @@
 
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-// This function was previously in a file that could not be resolved.
-// Moving it here fixes the "Module not found" error.
-function initializeFirebase() {
-    const firebaseConfig = {
-        apiKey: "AIzaSy...", 
-        authDomain: "verse-exam-prep.firebaseapp.com",
-        projectId: "verse-exam-prep",
-        storageBucket: "verse-exam-prep.appspot.com",
-        messagingSenderId: "SENDER_ID",
-        appId: "APP_ID"
-    };
-
-    const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const firestore = getFirestore(app);
-    
-    return { firebaseApp: app, auth, firestore };
-}
+import { initializeFirebase } from '@/firebase';
 
 
 interface FirebaseClientProviderProps {
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8465754929-f1e1f",
-  "appId": "1:738536341108:web:b4f668c1c34dad9c5d9b21",
-  "apiKey": "AIzaSyAa9Q9ugfETMSFnVX99sBcs1i25KEurzIk",
-  "authDomain": "studio-8465754929-f1e1f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "738536341108"
+    apiKey: "AIzaSyAa9Q9ugfETMSFnVX99sBcs1i25KEurzIk", // User will paste real API Key here
+    authDomain: "verse-exam-prep.firebaseapp.com",
+    projectId: "verse-exam-prep",
+    storageBucket: "verse-exam-prep.appspot.com",
+    messagingSenderId: "738536341108",
+    appId: "1:738536341108:web:b4f668c1c34dad9c5d9b21"
 };
